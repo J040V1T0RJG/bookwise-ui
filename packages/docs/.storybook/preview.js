1b766a701ed9c62b0f89ea1f9fdc758176add5d4
João Vitor <joaovitor.jovg73@gmail.com>
2023-05-25 19:02:50 -0300

    :bug: fix: deploy error => Failed to fetch dynamically imported module: https://j040v1t0rjg.github.io/bookwise-ui/assets/preview-0b573777.js

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,4 +1,6 @@
-/** @type { import('@storybook/react').Preview } */
+// /** @type { import('@storybook/react').Preview } */
+import React from '@storybook/react'
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
